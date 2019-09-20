33a3ba9dd67aaa5c0db77a043b10230094f35535
Tanvir Rahman <pathaoltd.@Tanvir.local>
2019-09-20 17:30:31 +0600

    banner,speaker,sessions and sponsors area done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-meetup19';
+  faTwitter = faTwitter;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+
+  constructor(@Inject(DOCUMENT) document) { }
+
+  ngOnInit() {  }
+
+  // @HostListener('window:scroll', ['$event'])
+  // onWindowScroll(e) {
+  //    if (window.pageYOffset > 100) {
+  //      let element = document.getElementById('navbar');
+  //      element.classList.add('sticky');
+  //    } else {
+  //     let element = document.getElementById('navbar');
+  //       element.classList.remove('sticky');
+  //    }
+  // }
 }
